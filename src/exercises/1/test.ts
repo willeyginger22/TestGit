56e53edfb09b421257b82ef9ef1fd4f8c44abeb0
willeyginger22 <chaitanyaramkumar@gmail.com>
2024-10-09 13:47:20 +0530

    cc

--- src/exercises/1/test.ts
+++ src/exercises/1/test.ts
@@ -2,8 +2,6 @@ import {IsTypeEqual, typeAssert} from 'type-assertions';
 import {User, logPerson, users} from './index';
 
 
-typeAssert<IsTypeEqual<User,{"c",3,"b"}>>();
-
 typeAssert<IsTypeEqual<User, {name: string, age: number, occupation: string}>>();
 typeAssert<IsTypeEqual<typeof users, {name: string, age: number, occupation: string}[]>>();
 typeAssert<IsTypeEqual<typeof logPerson, (user: {name: string, age: number, occupation: string}) => void>>();
